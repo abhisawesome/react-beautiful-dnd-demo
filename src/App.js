04be1935f1918e698f4369125c4f8b2ef4622ff0
Abhijith v <abhijithababhijith@gmail.com>
2021-07-10 14:43:49 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [{ id: "1", label: 'one' }, { id: "2", label: 'Two' }]
+const items1 = [{ id: '3', label: '3' }]
+const App = () => {
+    const grid = 8;
+    const [board, setBoard] = useState(items1);
+    const getItemStyle = (isDragging, draggableStyle) => ({
+        // some basic styles to make the items look a bit nicer
+        userSelect: 'none',
+        padding: grid * 2,
+        margin: `0 0 ${grid}px 0`,
+
+        // change background colour if dragging
+        background: isDragging ? 'lightgreen' : 'grey',
+
+        // styles we need to apply on draggables
+        ...draggableStyle
+    });
+
+    const getListStyle = isDraggingOver => ({
+        background: isDraggingOver ? 'lightblue' : 'lightgrey',
+        padding: grid,
+        width: 250
+    });
+
+    const onDragEnd = (result) => {
+        console.log('inn', { source: result.source, destination: result.destination, draggableId: result.draggableId });
+        const { destination, source, draggableId } = result;
+        if (source.droppableId === 'droppable2' && destination.droppableId === 'droppable1') {
+            let tempBoard = board;
+            tempBoard.push({ id: new Date().getTime(), label: items.find(item => item.id === draggableId).label })
+            setBoard(tempBoard);
+        }
+
+    }
+    return (<div>
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable2">
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        style={getListStyle(snapshot.isDraggingOver)}
+                    >
+                        {items.map((item, index) => (
+                            <Draggable
+
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}>
+                                {(provided, snapshot) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={getItemStyle(
+                                            snapshot.isDragging,
+                                            provided.draggableProps.style
+                                        )}
+                                    >
+                                        {item.label}
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+
+            ****************************************
+            <Droppable droppableId="droppable1">
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        style={getListStyle(snapshot.isDraggingOver)}
+                    >
+                        {board.map((item, index) => (
+                            <Draggable
+
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}>
+                                {(provided, snapshot) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={getItemStyle(
+                                            snapshot.isDragging,
+                                            provided.draggableProps.style
+                                        )}
+                                    >
+                                        {item.label}
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    </div>)
+};
 
 export default App;
